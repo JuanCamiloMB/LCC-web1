f27bc1a633080c70aa5fd44b100bbdd72137b6fb
Shinn Thant <94442302+DevShinnThant@users.noreply.github.com>
2024-10-20 08:44:40 +0630

    fix: scroll in screen loading stage

--- app/components/ScreenLoader.tsx
+++ app/components/ScreenLoader.tsx
@@ -1,18 +1,25 @@
 "use client"
 
 import { useEffect } from "react"
+import { disableScroll, enableScroll } from "@/lib/utils"
 import gsap from "gsap"
 
 export default function ScreenLoader() {
   useEffect(() => {
+    disableScroll()
+
     gsap.to(".loader-screen", {
       height: 0,
       duration: 0.6,
       delay: 2,
+
+      onComplete: () => {
+        enableScroll()
+      },
     })
   }, [])
 
   return (
-    <div className="loader-screen fixed z-50 flex h-screen w-full items-center justify-center overflow-hidden bg-black" />
+    <div className="loader-screen fixed z-50 flex h-screen w-full items-center justify-center bg-black" />
   )
 }
--- lib/utils.ts
+++ lib/utils.ts
@@ -4,3 +4,15 @@ import { twMerge } from "tailwind-merge";
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
+
+export const disableScroll = () => {
+  document.body.style.position = "fixed";
+  document.body.style.top = `-${window.scrollY}px`;
+};
+
+export const enableScroll = () => {
+  const scrollY = document.body.style.top;
+  document.body.style.position = "";
+  document.body.style.top = "";
+  window.scrollTo(0, parseInt(scrollY || "0") * -1);
+};
