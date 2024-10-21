6170c4056b1ac26113bbf939ffa77b72a67e1b10
Shinn Thant <94442302+DevShinnThant@users.noreply.github.com>
2024-10-21 13:30:10 +0630

    feat: statue section ui & animation

--- app/page.tsx
+++ app/page.tsx
@@ -4,6 +4,7 @@ import HeroSection from "./sections/HeroSection"
 import PinSection from "./sections/PinSection"
 import SliderSection from "./sections/SliderSection"
 import SpaceSection from "./sections/SpaceSection"
+import StatueSection from "./sections/StatueSection"
 
 export default function Home() {
   return (
@@ -14,64 +15,7 @@ export default function Home() {
       <CardsSection />
       <SpaceSection />
       <SliderSection />
-
-      <div className="relative z-10 h-screen w-full bg-pink-400 text-black">
-        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eos, quaerat
-        aperiam recusandae necessitatibus error corporis molestias asperiores
-        totam quae ex nam odit sit architecto, dignissimos aut praesentium
-        soluta exercitationem ullam sequi eligendi. Excepturi amet, ut officia
-        eveniet laborum deserunt nostrum doloremque et natus assumenda rerum at
-        itaque provident magni porro ipsam. Saepe quasi vel quam ratione atque,
-        ut similique eligendi velit possimus! Nesciunt quasi culpa saepe
-        reiciendis, doloremque architecto error doloribus totam, accusamus
-        corrupti beatae voluptatem sunt ullam et quia aperiam ad id, cum
-        excepturi. Nostrum rem nobis maxime aperiam maiores atque, nam dolore
-        ratione beatae ut ducimus deleniti sit temporibus facilis voluptates
-        praesentium. Rem unde amet doloremque culpa quaerat? Dolorum
-        voluptatibus, hic, voluptatum sint excepturi explicabo autem molestias
-        exercitationem quibusdam temporibus dolor praesentium. Aut eaque, in
-        facilis vero aliquam aliquid repellendus temporibus voluptates qui, ipsa
-        nihil facere voluptas hic odio quod nulla earum explicabo doloremque,
-        vitae atque officiis modi. Optio iste, culpa maxime facere perferendis
-        inventore veniam ex aliquam, assumenda maiores delectus! Recusandae
-        dolor, similique officia earum illum ab porro alias ex soluta quae
-        tempora vel debitis quas corrupti ducimus quidem saepe sunt eligendi sed
-        dignissimos! Enim eius nihil alias architecto. Repellat id earum
-        laudantium debitis enim, eius voluptatum expedita sunt doloribus quasi
-        voluptatem iusto repudiandae explicabo iste ipsa quia aut laboriosam
-        amet rem nostrum! Nesciunt cupiditate cum, neque odio dignissimos nulla
-        dicta dolore id debitis praesentium blanditiis nobis aliquid vitae
-        doloribus aut fuga quos illum omnis ex quae distinctio quod
-        necessitatibus reiciendis amet? Neque voluptatum ducimus saepe deserunt
-        enim itaque aspernatur voluptatem, labore aliquam unde laboriosam quo ea
-        facilis ipsam, quisquam repudiandae illum accusantium corporis autem
-        possimus. Quisquam totam officia doloremque sunt esse possimus cum
-        debitis molestias maiores voluptatibus hic, mollitia eveniet rem, atque
-        impedit corrupti delectus culpa placeat magni omnis libero ex.
-        Accusantium quam quae quibusdam quaerat pariatur nulla vel, ab aut
-        autem! Sed sit quidem exercitationem vitae temporibus, ipsam est soluta
-        sint! Unde maiores, iusto quidem consequuntur quibusdam repudiandae
-        libero laboriosam et soluta est nostrum quos reprehenderit, odit quaerat
-        ipsum? Alias, expedita exercitationem dolorem ab sunt quas provident.
-        Provident esse, consectetur vitae optio modi eaque soluta repellendus
-        eos a ea doloremque vero odio itaque. Ea dolorum amet temporibus
-        doloribus commodi mollitia in recusandae maiores iure officia
-        exercitationem architecto, quis possimus cupiditate rerum accusantium
-        reiciendis! Doloremque eius, deleniti tenetur dolor, laborum eveniet
-        labore beatae blanditiis accusamus quis optio fugiat aliquid soluta nemo
-        quas, temporibus obcaecati est? Libero, adipisci voluptas voluptates
-        iusto sunt odio facilis molestiae assumenda, inventore nostrum maxime
-        ducimus, quidem corporis sint voluptate dolorum nobis at incidunt dicta?
-        Repellat distinctio iure sit, sapiente aliquid fugiat itaque voluptatum
-        reiciendis, obcaecati sequi accusantium minima. Quos cupiditate ea,
-        sapiente quae fugiat autem architecto aliquam iusto, atque nesciunt,
-        sint dolores in hic nemo vel voluptas? Animi, excepturi, ad totam dolore
-        nesciunt officia quis dolores beatae sunt dignissimos illo sed. Tempora,
-        unde aspernatur? Vitae asperiores, ducimus necessitatibus minus aut
-        saepe culpa ab blanditiis. Sequi autem at ut non laudantium quibusdam
-        cupiditate dicta, culpa deserunt debitis accusantium porro obcaecati
-        eveniet, quaerat illo!
-      </div>
+      <StatueSection />
     </div>
   )
 }
---
+++ app/sections/StatueSection.tsx
@@ -0,0 +1,162 @@
+"use client"
+
+import { useEffect } from "react"
+import RobotStatueBg from "@/app/assets/robot-female-bg.jpg"
+import RobotStatue from "@/app/assets/robot-female.png"
+import { cn } from "@/lib/utils"
+import gsap from "gsap"
+import Image from "next/image"
+import SplitType from "split-type"
+import { reckoner } from "../fonts"
+
+export default function StatueSection() {
+  /* Texts */
+  useEffect(() => {
+    const text = new SplitType(".statue-text", {
+      types: "chars",
+      tagName: "span",
+    })
+
+    gsap.set(text.chars, {
+      yPercent: 500,
+    })
+
+    const tl = gsap.timeline({
+      defaults: {
+        stagger: 5,
+      },
+    })
+
+    tl.to(text.chars, {
+      yPercent: 0,
+      scrollTrigger: {
+        trigger: "#statue-text-section",
+        start: "top bottom",
+        markers: true,
+        endTrigger: "#statue-section",
+        end: "60% bottom",
+        scrub: true,
+      },
+      duration: 40.5,
+    })
+
+    gsap.to("#statue-text-section", {
+      scrollTrigger: {
+        trigger: "#statue-section",
+        start: "60% bottom",
+        endTrigger: "#statue-section",
+        end: "bottom top",
+        scrub: true,
+      },
+      xPercent: -100,
+    })
+
+    gsap.to("#statue-text-section", {
+      scrollTrigger: {
+        trigger: "#statue-text-section",
+        start: "top top",
+        endTrigger: "#statue-section",
+        end: "bottom top",
+        pin: true,
+      },
+    })
+  }, [])
+
+  /* Description */
+  useEffect(() => {
+    gsap.to("#statue-description-section", {
+      scrollTrigger: {
+        trigger: "#statue-description-section",
+        start: "top top",
+        endTrigger: "#statue-section",
+        end: "bottom top",
+        pin: true,
+      },
+    })
+
+    const statueDescriptionText = new SplitType(".statue-description", {
+      types: "chars",
+      tagName: "span",
+    })
+
+    const statueDescriptionTl = gsap.timeline({
+      defaults: {
+        stagger: 40,
+      },
+    })
+
+    const statueDescriptionTextWrapper = document.querySelector(
+      ".statue-description"
+    )
+    const statueDescriptionTextWrapperWidth =
+      statueDescriptionTextWrapper?.clientWidth || 0
+
+    gsap.set(statueDescriptionText.chars, {
+      x: statueDescriptionTextWrapperWidth,
+    })
+
+    statueDescriptionTl.to(statueDescriptionText.chars, {
+      x: 0,
+      duration: 100,
+      scrollTrigger: {
+        trigger: "#statue-section",
+        start: "55% bottom",
+        end: "65% bottom",
+        scrub: true,
+      },
+    })
+  }, [])
+
+  return (
+    <>
+      <section
+        id="statue-text-section"
+        className="absolute left-0 top-[780vh] z-30 h-screen w-full p-5"
+      >
+        <div
+          className={cn(
+            reckoner.className,
+            "statue-text h-screen w-screen text-[200px] font-extrabold leading-[1] tracking-wider text-white"
+          )}
+        >
+          LIVE WITH YR DREAM
+        </div>
+      </section>
+
+      <section
+        id="statue-description-section"
+        className="absolute left-0 top-[780vh] z-[41] h-screen w-full"
+      >
+        <div
+          className={cn(
+            reckoner.className,
+            "statue-description absolute left-1/2 top-1/2 w-fit -translate-x-1/2 -translate-y-1/2 overflow-hidden text-4xl tracking-wide text-white"
+          )}
+        >
+          LIVE WITH YOUR DREAM
+        </div>
+      </section>
+
+      <section
+        id="statue-section"
+        className="relative z-20 h-[420vh] w-full px-5 text-9xl text-white"
+      >
+        <Image
+          priority
+          alt="robot-statue"
+          className="absolute left-0 top-0 h-full w-full object-cover"
+          src={RobotStatueBg}
+        />
+      </section>
+
+      <section className="absolute left-0 top-[780vh] z-40 h-[420vh] w-full">
+        <Image
+          priority
+          alt="robot-statue"
+          className="absolute left-0 top-0 h-full w-full object-cover"
+          src={RobotStatue}
+        />
+      </section>
+    </>
+  )
+}
